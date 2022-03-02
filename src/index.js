d075644a01aa4f1552a17134747811ffcd06490a
nieopierzony <43653566+nieopierzony@users.noreply.github.com>
2022-03-03 02:20:54 +0300

    Load scenes loader on bot start

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 const path = require('path');
 const { Telegraf } = require('telegraf');
 const TelegrafI18n = require('telegraf-i18n');
+const session = require('telegraf/session');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
@@ -13,7 +14,9 @@ const i18n = new TelegrafI18n({
   directory: path.resolve(__dirname, 'locales'),
 });
 bot.use(i18n.middleware());
+bot.use(session());
 
+require('./loaders/loadScenes')(bot);
 require('./loaders/loadHandlers')(bot);
 
 bot.launch();
