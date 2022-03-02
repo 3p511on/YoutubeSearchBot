fd8ff22dc4e6337d81ad9fdc8e8be1e5c7910e79
nieopierzony <43653566+nieopierzony@users.noreply.github.com>
2022-03-02 23:54:32 +0300

    Rename Telegram class to send

---
+++ src/utilities/send.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const Extra = require('telegraf/extra');
+
+const send = async (ctx, data) => {
+  const { content, keyboard } = data;
+  const markup = keyboard ? keyboard.extra(Extra.HTML()) : { parse_mode: 'HTML' };
+  try {
+    const hasPhoto = ctx.update?.callback_query?.message?.photo;
+    if (ctx.message || hasPhoto) {
+      if (hasPhoto) await ctx.editMessageReplyMarkup();
+      const msg = await ctx.replyWithHTML(content, markup);
+      return msg;
+    } else {
+      await ctx.answerCbQuery();
+      await ctx.editMessageText(content, markup);
+    }
+  } catch (err) {
+    if (err.message.includes('message is not modified')) return false;
+    console.error(err);
+  }
+  return true;
+};
+
+module.exports = send;
