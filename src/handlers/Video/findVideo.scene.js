d2b5bf38ea9181e6225da376660cf42cdc6da0e2
nieopierzony <43653566+nieopierzony@users.noreply.github.com>
2022-03-04 00:24:54 +0300

    Fully find video and show information about it

--- src/handlers/Video/findVideo.scene.js
+++ src/handlers/Video/findVideo.scene.js
@@ -1,6 +1,8 @@
 'use strict';
 
-const { extractID } = require('../../utilities/youtube');
+const { backKeyboard } = require('../../keyboards/back');
+const { videoInfoKeyboard } = require('../../keyboards/videoInfo');
+const { extractID, getSearchResults } = require('../../utilities/youtube');
 
 module.exports = {
   data: {
@@ -8,15 +10,35 @@ module.exports = {
     type: 'wizard',
   },
   methods: [
-    (ctx) =>
-      // TODO: Leave button
-      ctx.i18n.t('findVideo.provideLink'),
+    (ctx) => ({
+      content: ctx.i18n.t('findVideo.provideLink'),
+      keyboard: backKeyboard(ctx.i18n),
+    }),
     (ctx) => {
+      if (!ctx?.message?.text) return null;
       const id = extractID(ctx.message.text);
       if (!id) return new Error('findVideo.invalidLink');
-      console.log(id);
       ctx.session.videoID = id;
-      return { end: true };
+      return {
+        content: ctx.i18n.t('findVideo.provideQuery'),
+        keyboard: backKeyboard(ctx.i18n),
+      };
+    },
+    async (ctx) => {
+      if (!ctx?.message?.text) return null;
+      ctx.session.searchQuery = ctx.message.text;
+      const { videoID, searchQuery } = ctx.session;
+      const userCookies = ctx?.user?.authData?.cookies;
+      const searchResults = await getSearchResults(searchQuery, userCookies);
+      const videoInfo = searchResults.find((r) => r.id === videoID);
+      if (!videoInfo) return new Error('findVideo.videoNotFound', { searchQuery });
+      const position = searchResults.findIndex((video) => video.id === videoID) + 1;
+      const templateVariables = { videoInfo, searchQuery, position };
+      return {
+        content: ctx.i18n.t('findVideo.videoInfo', templateVariables),
+        keyboard: videoInfoKeyboard(ctx.i18n),
+        end: true,
+      };
     },
   ],
 };
